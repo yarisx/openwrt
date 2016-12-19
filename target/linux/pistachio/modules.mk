adbc3bb37f47236960671ed17e05617deb637035
Abhijit Mahajani <Abhijit.Mahajani@imgtec.com>
2016-12-19 12:06:53 +0530

    pistachio/modules.mk: add openwrt license header
    Signed-off-by: Abhijit Mahajani <Abhijit.Mahajani@imgtec.com>

--- target/linux/pistachio/modules.mk
+++ target/linux/pistachio/modules.mk
@@ -1,3 +1,9 @@
+#
+# Copyright (C) 2016 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
 
 define KernelPackage/serial-sc16is7xx
   TITLE:= SC16IS7xx Serial driver
